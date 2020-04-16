338f8fdc65e3fca1b6e589245f03eb6877233c04
Jakob S Kottmann <jakob.kottmann@gmail.com>
2020-04-16 12:06:32 -0400

    changed qulacstest to module

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,9 @@ import os
 setuptools.setup(
     name="qulacs-test",
     version="0.1.0",
-    py_modules=['qulacstest'],
+    py_modules=['mymodule'],
+    package_dir={'': 'src'},
+    packages=find_packages(include=['src','src/mymodule']),
     install_requires=[
       #  'z-quantum-core',
         'cmake',
